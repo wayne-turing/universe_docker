0aaf1c8a42338e67b705aa5e9e9f8f208603b31d
wayne <wayne@turing-drive.com>
2023-08-25 13:38:42 +0800

    add common

--- 0_insatll_nomachine.sh
+++ 0_insatll_nomachine.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+
 sudo apt-get install -y ubuntu-desktop
 
 wget https://download.nomachine.com/download/8.8/Linux/nomachine_8.8.1_1_amd64.deb
--- 4_prepare_autoware.sh
+++ 4_prepare_autoware.sh
@@ -2,6 +2,10 @@
 
 sudo apt install -y zip
 
+cd 
+
+mkdir autoware_map
+
 gdown -O ~/autoware_map/ 'https://docs.google.com/uc?export=download&id=1499_nsbUbIeturZaDj7jhUownh5fvXHd'
 
 unzip -d ~/autoware_map ~/autoware_map/sample-map-planning.zip
