89b125b0817fde9b9a45d2996b8430f7780146e7
wayne-turing <69493143+wayne-turing@users.noreply.github.com>
2023-08-25 21:45:50 +0800

    update run autoware shell

--- 4_prepare_autoware.sh
+++ 4_prepare_autoware.sh
@@ -5,7 +5,7 @@ sudo apt install -y zip
 cd 
 
 mkdir autoware_map
-
+#first sample map
 gdown -O ~/autoware_map/ 'https://docs.google.com/uc?export=download&id=1499_nsbUbIeturZaDj7jhUownh5fvXHd'
 
 unzip -d ~/autoware_map ~/autoware_map/sample-map-planning.zip
