adf76a90d7875fa6bb167f38d64c423bf32cb410
wayne-turing <69493143+wayne-turing@users.noreply.github.com>
2023-08-27 18:19:38 +0800

    ADD ROS HUMBLE SHELL

---
+++ Install_ros_humble.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Set locale
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+#Setup Sources
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+#Install ROS 2 packages
+sudo apt update
+sudo apt install -y ros-humble-desktop
+
+#Sourcing the setup script
+echo '' >> ~/.bashrc && echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+source /opt/ros/humble/setup.bash
