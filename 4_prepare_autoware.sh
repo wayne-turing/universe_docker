66fa0dbda72e73644f50b25483ed7ceeebb9d6c1
wayne-turing <69493143+wayne-turing@users.noreply.github.com>
2023-08-27 23:20:42 +0800

    update txt data and shell

--- 4_prepare_autoware.sh
+++ 4_prepare_autoware.sh
@@ -7,5 +7,7 @@ cd
 mkdir autoware_map
 #first sample map
 gdown -O ~/autoware_map/ 'https://docs.google.com/uc?export=download&id=1499_nsbUbIeturZaDj7jhUownh5fvXHd'
-
 unzip -d ~/autoware_map ~/autoware_map/sample-map-planning.zip
+
+gdown -O ~/autoware_map/ 'https://github.com/tier4/AWSIM/releases/download/v1.1.0/nishishinjuku_autoware_map.zip'
+unzip -d ~/autoware_map ~/autoware_map/nishishinjuku_autoware_map.zip
